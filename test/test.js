42251202067f464000bee118781260dee471b6e4
andcastillo <andcastillo@gmail.com>
2015-10-13 12:06:50 -0500

    FFT and IFFT test case
    Test for FFT and IFFT working

--- test/test.js
+++ test/test.js
@@ -1,9 +1,26 @@
 'use strict';
+var FFTUtils = require("../src/FFTUtils");
 
-var fft = require('..');
+describe('FFT', function () {
+    it('should return the same', function () {
+        var n = 4;
+        var nRows = n;
+        var nCols = n;
+        var data = new Array(nRows*nCols);
+        for(var i=0;i<nRows;i++){
+            for(var j=0;j<nCols;j++){
+                data[i*nCols+j]=i+j;
+            }
+        }
+        var ftData = FFTUtils.fft2DArray(data, nCols, nCols);
+        var ftRows = nRows * 2;
+        var ftCols = nCols / 2 + 1;
 
-describe('test', function () {
-    it('should return x', function () {
-        fft.x().should.equal('x');
+        var iftData =  FFTUtils.ifft2DArray(ftData, ftRows, ftCols);
+        for(var i=0;i<nRows;i++){
+            for(var j=0;j<nCols;j++){
+                data[i*nCols+j].should.be.equal(iftData[i*nCols+j]);
+            }
+        }
     });
 });
